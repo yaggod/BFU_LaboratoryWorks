50a8cb5a8c1805273b10da4c7e9735552406eb96
yaggod <yaggod@yandex.ru>
2023-09-10 12:34:35 +0300

    Добавьте файлы проекта.

---
+++ LaboratoryWork_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaboratoryWork_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
