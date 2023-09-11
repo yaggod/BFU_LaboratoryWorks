98785a2d7aa367c6f89a238c94a18d0ddcb17273
student24 <student24@02F2R230C09FIZM>
2023-09-11 14:48:35 +0200

    Выполнена лабораторная работа №3

---
+++ LaboratoryWork_3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LaboratoryWork_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальное число: ");
+            long maxValue = Int64.Parse(Console.ReadLine() ?? "0");
+
+            List<long> suitableValues = ValueFinder.GetSuitableNumbers(maxValue);
+            suitableValues.Sort();
+            Console.WriteLine(String.Join('\n', suitableValues));
+        }
+    }
+}
---
+++ LaboratoryWork_3/ValueFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_3
+{
+    internal class ValueFinder
+
+    {
+        private static readonly long _firstPowerBase = 3;
+        private static readonly long _secondPowerBase = 5;
+        private static readonly long _thirdPowerBase = 7;
+        public static List<long> GetSuitableNumbers(long maxValue)
+        {
+            List<long> result = new List<long>();
+            long firstValue, secondValue, thirdValue;
+            long production = 1;
+            for (firstValue = 1; firstValue <= maxValue; firstValue *= _firstPowerBase)
+                for(secondValue = 1; secondValue <= maxValue; secondValue *= _secondPowerBase)
+                    for(thirdValue = 1;thirdValue <= maxValue; thirdValue *= _thirdPowerBase)
+                    {
+                        production = firstValue * secondValue * thirdValue;
+                        if (production > maxValue)
+                            break;
+                        result.Add(production);
+                    }
+
+            return result;
+        }
+    }
+}
