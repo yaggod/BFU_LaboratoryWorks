1fc89a9775b6ec531431ec0d5c426720b477a10c
yaggod <yaggod@yandex.ru>
2023-10-01 22:43:49 +0300

    Выполнена лабораторная работа №6

---
+++ LaboratoryWork_6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LaboratoryWork_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var arrayToSort = Enumerable.Range(0, 20).Select(index => random.Next(-10000, 10000)).ToArray();
+
+            SelectionSort.Sort(arrayToSort);
+
+
+            Console.WriteLine(String.Join('\n', arrayToSort));
+        }
+    }
+}
---
+++ LaboratoryWork_6/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_6
+{
+    internal class SelectionSort
+    {
+        public static void Sort<T>(T[] values) where T: IComparable<T>
+        {
+            for(int i = 0; i < values.Length; i++)
+            {
+                int minimalValueIndex = i;
+                for(int j = i + 1; j < values.Length; j++)
+                {
+                    if (values[minimalValueIndex].CompareTo(values[j]) > 0)
+                        minimalValueIndex = j;
+                }
+
+                T temp = values[i];
+                values[i] = values[minimalValueIndex];
+                values[minimalValueIndex] = temp;
+            }
+        }
+    }
+}
