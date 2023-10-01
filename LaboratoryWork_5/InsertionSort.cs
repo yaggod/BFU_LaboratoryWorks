087b82c3561fa622b1cc43eeefeaf10a3d587887
yaggod <yaggod@yandex.ru>
2023-10-01 22:29:19 +0300

    Выполнена лабораторная работа №5

---
+++ LaboratoryWork_5/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_5
+{
+    internal class InsertionSort
+    {
+        public static void Sort<T>(T[] values) where T : IComparable<T>
+        {
+            for (int i = 1; i < values.Length; i++)
+            {
+                T currentValue = values[i];
+                int j;
+                for(j = i - 1; j >= 0 && values[j].CompareTo(currentValue) > 0; j--)
+                {
+                    if (currentValue.CompareTo(values[j]) < 0)
+                    {
+                        values[j + 1] = values[j];
+                    }
+                }
+                values[j+1] = currentValue;
+                
+            }
+        }
+    }
+}
---
+++ LaboratoryWork_5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LaboratoryWork_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var arrayToSort = Enumerable.Range(0, 100).Select(index => random.Next(10000)).ToArray();
+
+            InsertionSort.Sort(arrayToSort);
+            
+
+            Console.WriteLine(String.Join('\n', arrayToSort));
+        }
+    }
+}
