fca6a47635227a33561db621c8489fb0cb35dafd
yaggod <yaggod@yandex.ru>
2023-10-23 23:42:13 +0300

    начата работа над лр №11

---
+++ LaboratoryWork_11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LaboratoryWork_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var arrayToSort = Enumerable.Range(0, 40).Select(index => random.Next(-10000, 10000)).ToArray();
+
+            QuickSort.Sort(arrayToSort);
+
+            Console.WriteLine(String.Join('\n', arrayToSort));
+        }
+    }
+}
---
+++ LaboratoryWork_11/QuickSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_11
+{
+    internal class QuickSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        public static void Sort<T>(T[] array, int from, int to) where T : IComparable<T>
+        {
+            int middle = from + (to - from) / 2;
+            T pivot = array[middle];
+
+            int leftIndex = from;
+            int rightIndex = to;
+
+            while(leftIndex <= rightIndex)
+            {
+                while (array[leftIndex].CompareTo(pivot) < 0)
+                    leftIndex++;
+                while (array[rightIndex].CompareTo(pivot) > 0)
+                    rightIndex--;
+
+                if(leftIndex <= rightIndex)
+                {
+                    (array[leftIndex], array[rightIndex]) = (array[rightIndex], array[leftIndex]);
+                    leftIndex++;
+                    rightIndex--;
+                }
+
+
+            }
+
+        }
+    }
+}
