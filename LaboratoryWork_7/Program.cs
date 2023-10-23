d68452e7beff98388f5882eecba615b1f09a4f5d
yaggod <yaggod@yandex.ru>
2023-10-23 06:08:35 +0300

    Выполнена работа 7

---
+++ LaboratoryWork_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LaboratoryWork_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            //var arrayToSort = Enumerable.Range(0, 20).Select(index => random.Next(-10000, 10000)).ToArray();
+            var arrayToSort = Enumerable.Range(0, 20).Select(index => -index).ToArray();
+
+            ShellSort.Sort(arrayToSort);
+
+
+            Console.WriteLine(String.Join('\n', arrayToSort));
+        }
+    }
+}
---
+++ LaboratoryWork_7/ShellSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_7
+{
+    internal class ShellSort
+    {
+        public static void Sort<T>(T[] values) where T : IComparable<T>
+        {
+            for(int step = values.Length / 2; step > 0; step /= 2)
+            {
+                for(int i = step; i < values.Length; i++)
+                {
+                    T currentValue = values[i];
+                    int j;
+
+                    for(j = i - step; j >=0; j -= step)
+                    {
+                        if (currentValue.CompareTo(values[j]) > 0)
+                        {
+                            break;
+                        }
+                        values[j + step] = values[j];
+                    }
+                    values[j + step] = currentValue;
+                }
+            }
+        }
+    }
+}
+
+
+/*
+for (int i = 1; i < values.Length; i++)
+            {
+                T currentValue = values[i];
+                int j;
+                for(j = i - 1; j >= 0; j--)
+                {
+                    if (currentValue.CompareTo(values[j]) > 0)
+                    {
+                        break;
+                    }
+                    values[j + 1] = values[j];
+                }
+
+                values[j+1] = currentValue;
+                
+            } 
+*/
