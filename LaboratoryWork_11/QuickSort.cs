f320276501d14d2c21f980bdfca6700d5eec91f6
yaggod <yaggod@yandex.ru>
2023-11-08 17:32:18 +0300

    LW9 started from scratch

--- LaboratoryWork_11/QuickSort.cs
+++ LaboratoryWork_11/QuickSort.cs
@@ -16,29 +16,7 @@ namespace LaboratoryWork_11
 
         public static void Sort<T>(T[] array, int from, int to) where T : IComparable<T>
         {
-            int middle = from + (to - from) / 2;
-            T pivot = array[middle];
-
-            int leftIndex = from;
-            int rightIndex = to;
-
-            while(leftIndex <= rightIndex)
-            {
-                while (array[leftIndex].CompareTo(pivot) < 0)
-                    leftIndex++;
-                while (array[rightIndex].CompareTo(pivot) > 0)
-                    rightIndex--;
-
-                if(leftIndex <= rightIndex)
-                {
-                    (array[leftIndex], array[rightIndex]) = (array[rightIndex], array[leftIndex]);
-                    leftIndex++;
-                    rightIndex--;
-                }
-
-
-            }
-
+            throw new NotImplementedException();
         }
     }
 }
