a4fc399a63a83f62c0f5a5492db139ef2de6726f
yaggod <yaggod@yandex.ru>
2023-11-08 20:44:46 +0300

    lw13 finished

---
+++ LaboratoryWork_13/HashTable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork_13
+{
+    internal class HashTable<T>
+    {
+        private readonly int _hashBase;
+        private readonly Func<T, int> _hashFunction;
+        private object[] _values;
+
+
+        public HashTable(Func<T, int> hashFunction, int hashBase)
+        {
+            if (hashBase < 1)
+                throw new ArgumentException(nameof(hashBase) + " can not be less than 1");
+
+            _hashFunction = hashFunction;
+            _hashBase = hashBase;
+
+            _values = new object[hashBase];
+        }
+
+        public void Add(T item)
+        {
+            object objectToAdd = item as object;
+            int hash = GetHashCode(item);
+
+            int index = hash;
+            while(_values[index] != null)
+            {
+                index++;
+                if (index >= _values.Length)
+                    ExpandArray();
+            }
+            _values[index] = objectToAdd;
+        }
+
+        private void ExpandArray()
+        {
+            object[] newValues = new object[_values.Length * 2];
+            for(int i = 0; i < _values.Length; i++)
+                newValues[i] = _values[i];
+
+            _values = newValues;
+        }
+
+        public bool Contains(T item)
+        {
+            int hash = GetHashCode(item);
+            for(int i = hash; i < _values.Length; i++)
+            {
+                if (_values[i].Equals(item))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public T[] GetValues()
+        {
+            return _values.Select(item => (T)item).ToArray();
+        }
+
+        private int GetHashCode(T item)
+        {
+            return Mod(_hashFunction(item), _hashBase);
+        }
+
+        private static int Mod(int value, int hashBase)
+        {
+            return (value % hashBase + hashBase) % hashBase;
+        }
+    }
+}
---
+++ LaboratoryWork_13/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace LaboratoryWork_13
+{
+    internal class Program
+    {
+        private const int HashBase = 32;
+        private const int TabCharacterLength = 7;
+
+        static void Main(string[] args)
+        {
+            HashTable<string> hashTable = GetInitializedHashTable();
+
+            IEnumerable<string> tableRows = hashTable.GetValues().Select((item, index) => $"{index}\t|\t{item}");
+
+            int longestRow = tableRows.Max(x => x.Length) + TabCharacterLength * 2;
+            string rowSeparator = String.Concat(Enumerable.Repeat('-', longestRow));
+            using (StreamWriter writer = File.CreateText("output.txt"))
+            {
+                foreach (string tableRow in tableRows)
+                {
+                    writer.WriteLine(rowSeparator);
+                    writer.WriteLine(tableRow);
+                }
+                writer.WriteLine(rowSeparator);
+            }
+        }
+
+        private static HashTable<string> GetInitializedHashTable()
+        {
+            string input = File.ReadAllText("input.txt", System.Text.Encoding.UTF8);
+            string[] words = input.Split(new[] { '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            Func<string, int> hashFunction = (s) => {
+                return s.First();
+            };
+            HashTable<string> hashTable = new HashTable<string>(hashFunction, HashBase);
+            foreach (string word in words)
+            {
+                hashTable.Add(word);
+            }
+
+            return hashTable;
+        }
+
+    }
+}
