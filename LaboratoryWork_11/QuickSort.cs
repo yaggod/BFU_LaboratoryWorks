f5e620ecc447d117b2e8c3d684a5c6bf86cf1043
yaggod <yaggod@yandex.ru>
2023-11-08 18:32:43 +0300

    lw11 finished

--- LaboratoryWork_11/QuickSort.cs
+++ LaboratoryWork_11/QuickSort.cs
@@ -4,19 +4,50 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace LaboratoryWork_11
 {
     internal class QuickSort
     {
-        public static void Sort<T>(T[] array) where T : IComparable<T>
+        public static void Sort<T>(T[] values) where T : IComparable<T>
         {
-            Sort(array, 0, array.Length - 1);
+            Sort(values, 0, values.Length - 1);
         }
 
-        public static void Sort<T>(T[] array, int from, int to) where T : IComparable<T>
+        public static void Sort<T>(T[] values, int from, int to) where T : IComparable<T>
         {
-            throw new NotImplementedException();
+            if(from >= to)
+            {
+                return;
+            }
+
+
+
+            int pivotIndex = (from + to) / 2;
+            T pivot = values[pivotIndex];
+
+
+            int leftIndex = from;
+            int rightIndex = to;
+
+            while(leftIndex < rightIndex)
+            {
+                while (values[leftIndex].CompareTo(pivot) < 0)
+                    leftIndex++;
+                while (values[rightIndex].CompareTo(pivot) > 0)
+                    rightIndex--;
+
+                if(leftIndex <= rightIndex)
+                {
+                    (values[leftIndex], values[rightIndex]) = (values[rightIndex], values[leftIndex]);
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+
+            Sort(values, from, rightIndex);
+            Sort(values, leftIndex, to);
         }
     }
 }
