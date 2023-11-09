fdcd8cad090d49cfba5cb8255b27257cc6bb2ff3
yaggod <yaggod@yandex.ru>
2023-11-09 15:53:46 +0300

    started lw12

---
+++ LaboratoryWork_12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaboratoryWork_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExternalHeapSort heapSorter = new ExternalHeapSort("input.txt", 32);
+        }
+    }
+}
