1f5f89cfbcc42d68bfbfb4bda7a8ed09ab3fd5a4
yaggod <yaggod@yandex.ru>
2023-11-20 21:03:20 +0300

    implemented random files generation

--- LaboratoryWork_12/Program.cs
+++ LaboratoryWork_12/Program.cs
@@ -2,8 +2,27 @@
 {
     internal class Program
     {
+        private const int Million = 1 << 20;
         static void Main(string[] args)
         {
+            CreateBinaryFile("input.bin", Million);
+
+            ExternalMergeSort sorter = new("input.bin", "output.bin", 64);
+        }
+
+        private static void CreateBinaryFile(string fileName, int elementsCount)
+        {
+            Random random = new();
+            using(FileStream stream = File.OpenWrite(fileName))
+            {
+                BinaryWriter writer = new BinaryWriter(stream);
+                for(int  i = 0; i < elementsCount; i++)
+                {
+                    writer.Write(random.Next());
+                }
+                writer.Dispose();
+            }
         }
     }
+
 }
