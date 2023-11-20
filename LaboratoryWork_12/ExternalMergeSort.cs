5cfe77d9cf12c10174c836021bcf9ad364087e3b
yaggod <yaggod@yandex.ru>
2023-11-20 21:03:06 +0300

    fixed typo

--- LaboratoryWork_12/ExternalMergeSort.cs
+++ LaboratoryWork_12/ExternalMergeSort.cs
@@ -1,4 +1,6 @@
-﻿namespace LaboratoryWork_12
+﻿using System.Reflection;
+
+namespace LaboratoryWork_12
 {
     internal class ExternalMergeSort
     {
@@ -32,10 +34,41 @@
 
         private void SplitIntoParts()
         {
+            BinaryReader reader = new BinaryReader(inputFileReader);
+            BinaryWriter writer = new BinaryWriter(tempFileWriter);
             for(int offset = 0; offset < inputFileReader.Length; offset += bufferSize)
             {
+                int[] buffer = new int[bufferSize];
 
+
+                int realBufferSize;
+                for(realBufferSize = 0; realBufferSize < bufferSize; realBufferSize++)
+                {
+                    buffer[realBufferSize] = reader.ReadInt32();
+                    if (writer.BaseStream.Position == writer.BaseStream.Length)
+                        break;
+                }
+
+                if(realBufferSize != bufferSize)
+                {
+                    int[] tempBuffer = new int[realBufferSize];
+                    for(int i = 0; i < tempBuffer.Length; i++)
+                    {
+                        tempBuffer[i] = buffer[i];
+                    }
+                    buffer = tempBuffer;
+
+                }
+
+                Array.Sort(buffer);
+                foreach(int element in buffer)
+                {
+                    writer.Write(element);
+                }
             }
+
+            reader.Dispose();
+            writer.Dispose();
         }
 
         
